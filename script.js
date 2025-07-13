0afd96e8439a33c67789db8659611d7db1afa0f3
MarkoDev-Apps <ivanovic.sdet@gmail.com>
2025-07-12 21:26:02 -0700

    Configuring game for public deployment.

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ function updateInfo() {
 }
 
 function cellClick(e) {
+if (gameOver || board[i]) return;
   const i = e.target.dataset.i;
   if (gameOver || board[i]) return;
   board[i] = current;
